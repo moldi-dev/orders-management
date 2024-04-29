1aee063f000c98fb36e031577fbffd49a8aed188
moldi-dev <sumalamadingdong@gmail.com>
2024-04-29 14:41:00 +0300

    v1.0.1 -> Removed the developer mode. Application fully functional and tested.

--- src/main/java/main/Application.java
+++ src/main/java/main/Application.java
@@ -3,17 +3,13 @@ package main;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import model.User;
-import session.SessionFactory;
 
 import java.io.IOException;
 
 public class Application extends javafx.application.Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/view/sign-in-view.fxml"));
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/view/admin-panel-view.fxml"));
-        SessionFactory.setSignedInUser(new User(1L, "admin", "admin", "t", "t", "t", "t", "t", "ADMINISTRATOR"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("/view/sign-in-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1280, 720);
         stage.setResizable(false);
         stage.setTitle("Orders management application");
